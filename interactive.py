d16b751f1245c8b6d512b44594a743247dcff2bc
natalie-s11 <teg9gu@virginia.edu>
2026-01-15 22:05:32 -0500

    Edit README.md

--- interactive.py
+++ interactive.py
@@ -4,11 +4,16 @@
 # Yes, the terminal display "path" is different
 # (.venv) @natalie-s11 ➜ /workspaces/daytwostuff (main) $ 
 
-# There are a lot of data preview/viewing extensions
-#
+# There are a lot of data preview/viewing extensions.
+# Three useful elements of Data Wrangler are:
+# 1. You can see the data in a table format.
+# 2. It can filter and sort the data quickly.
+# 3. Pandas code is automatically created for data changes.
+
+# Version: 6.5.1
+# We use a requirements.txt file because it lists all the packages and versions used in the project in one file which makes it easy to find and update.
 
 
 
 
 
-import pandas as pd
